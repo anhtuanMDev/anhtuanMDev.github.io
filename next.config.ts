fa2e4272523fe6508186951da6601e0b58f481aa
anhtuanMDev <anhtuan03.MDev@gmail.com>
2025-08-14 09:25:22 +0700

    save change

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,17 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
     output: "export",
     trailingSlash: true,
-    basePath: '/my-portfolio',
-    assetPrefix: '/my-portfolio/',
     images: {
         unoptimized: true,
     },
-    /* config options here */
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/,
-            use: ['@svgr/webpack'],
+            use: ["@svgr/webpack"],
         });
         return config;
     },
